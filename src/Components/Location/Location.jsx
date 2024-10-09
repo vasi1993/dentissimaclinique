1c1c3ffd6bc7fa9c2c18660d591dbc758c03bae3
vasi1993 <vasymoldovan93@gmail.com>
2024-10-09 16:55:18 +0300

    update map

--- src/Components/Location/Location.jsx
+++ src/Components/Location/Location.jsx
@@ -27,7 +27,7 @@ const Location = () => {
       </div>
       <div className="location-map">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2743.581790644449!2d23.813542476034133!3d46.5559591599369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47496647d014e50f%3A0x115de0e04e1f11ef!2sbl%20A2%2C%20ap%2011%2C%20Strada%20Lotus%2024%2C%20Turda%20401043!5e0!3m2!1sro!2sro!4v1721810236863!5m2!1sro!2sro"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10974.195997186398!2d23.79748656922195!3d46.55660773780237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x474967004256c081%3A0x41bafa6e87737067!2sDentissima%20Clinique!5e0!3m2!1sro!2sro!4v1728481991995!5m2!1sro!2sro"
           width="1200"
           height="450"
           allowFullScreen=""
